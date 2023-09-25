47d42048e5aef98527a39a2d5882202d089cb7e9
SamirL <sam.lachgar@gmail.com>
2023-09-25 14:42:36 +0400

    Updated packages versions and fixes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import cheerio from 'cheerio';
-import { fchownSync, fstat, writeFileSync } from 'fs';
-import _ from 'lodash';
-import request from 'request';
-import RequestPromise from 'request-promise-native';
-import Url from 'url';
-import { IAmazonObject } from './interfaces/amazon';
+import cheerio from 'cheerio'
+import { fchownSync, fstat, writeFileSync } from 'fs'
+import _ from 'lodash'
+import request from 'request'
+import RequestPromise from 'request-promise-native'
+import Url from 'url'
+import { IAmazonObject } from './interfaces/amazon'
 
 enum ERROR_TYPE {
   REQUEST_FAILED = 'REQUEST_FAILED',
@@ -17,42 +17,50 @@ enum ERROR_TYPE {
 }
 
 interface IAmazonForm {
-  formUrl: string;
-  formData: object;
+  formUrl: string
+  formData: object
 }
 
 interface IRequestObject {
-  url: string;
-  method: string;
-  formData?: object;
+  url: string
+  method: string
+  formData?: object
+}
+
+interface IAdditionalInformations {
+  createdOn: string
+  salesRank: object[]
+  ratings: object
+  categories: string[]
 }
 
 class AmazonScraper {
-  private cookieJar: any;
-  private productUrl: string;
+  private cookieJar: any
+  private productUrl: string
 
-  private readonly titleSelector: string = 'h1#title';
-  private readonly merchantIdSelector: string = '#merchantID';
-  private readonly merchantNameSelector: string = '#sellerProfileTriggerId';
-  private readonly addToCardBtnSelectors: string[] = ['#addToCart', '#handleBuy'];
+  private readonly titleSelector: string = 'h1#title'
+  private readonly merchantIdSelector: string = '#merchantID'
+  private readonly merchantNameSelector: string = '#sellerProfileTriggerId:first'
+  private readonly addToCardBtnSelectors: string[] = ['#addToCart', '#handleBuy']
   private readonly priceSelectors: string[] = [
     '#priceblock_ourprice',
     '#priceblock_saleprice',
     '#priceBlock .priceLarge',
     '#actualPriceContent .priceLarge',
     '#actualPriceValue .priceLarge',
-  ];
-  private readonly salesRankSelector = '.zg_hrsr_item .zg_hrsr_rank';
-  private readonly categorySelector = '#SalesRank a';
-  private readonly alertBoxSelector = '#huc-v2-box-warning';
-  private readonly confirmTextSelector = '#confirm-text';
-  private readonly remainingInventorySelector = '#hucArgsNewItems';
-  private readonly isAvailableSelector: string[] = ['#outOfStock', '#availability', '.availRed'];
-  private readonly defaultQuantity: number = 999;
+    '.a-price',
+  ]
+  private readonly salesRankSelector = '.zg_hrsr_item .zg_hrsr_rank'
+  private readonly categorySelector = '#SalesRank a'
+  private readonly alertBoxSelector = '#huc-v2-box-warning'
+  private readonly confirmTextSelector = '#confirm-text'
+  private readonly remainingInventorySelector = '#hucArgsNewItems'
+  private readonly isAvailableSelector: string[] = ['#outOfStock', '#availability', '.availRed']
+  private readonly defaultQuantity: number = 999
 
-  constructor(productUrl: string) {
-    this.cookieJar = request.jar();
-    this.productUrl = productUrl;
+  constructor (productUrl: string) {
+    this.cookieJar = request.jar()
+    this.productUrl = productUrl
   }
 
   /**
@@ -68,71 +76,72 @@ class AmazonScraper {
    * @param url   The Url to access.
    * @returns       A Cheerio Selector.
    */
-  public async accessPage(object: IRequestObject): Promise<CheerioSelector> {
+  public async accessPage (object: IRequestObject): Promise<any> {
     try {
-      const $: CheerioSelector = await RequestPromise({
+      const $: any = await RequestPromise({
         uri: object.url,
         form: object.formData,
         jar: this.cookieJar,
         method: object.method,
         followAllRedirects: true,
         resolveWithFullResponse: true,
-        transform(body: any) {
-          return cheerio.load(body);
+        transform (body: any) {
+          return cheerio.load(body)
         },
-      });
+      })
 
-      return $;
+      return $
     } catch (error) {
-      throw new Error(error);
+      throw new Error(error)
     }
   }
 
   /**
    * Return a Product's Information
    *
-   * @param {CheerioSelector} $
+   * @param {any} $
    * @returns {Promise<IAmazonObject>}
    * @memberof AmazonScraper
    */
-  public async processProductPage($: CheerioSelector): Promise<IAmazonObject> {
-    const title = this.scrapeProductTitle($);
-    const price = this.scrapeProductPrice($);
-    const merchantId = this.scrapeMerchantId($);
-    const category = this.scrapeCategory($);
-    const merchantName = this.scrapeMerchantName($);
-    const isAvailable = this.scrapeProductAvailability($);
-    const salesRank = this.scrapeSalesRank($);
-    const asin = this.scrapeProductAsin($);
+  public async processProductPage ($: any): Promise<IAmazonObject> {
+    const title = this.scrapeProductTitle($)
+    const price = this.scrapeProductPrice($)
+    const merchantId = this.scrapeMerchantId($)
+    const additionalInfo = this.scrapeAdditionalInfo($)
+    const merchantName = this.scrapeMerchantName($)
+    const isAvailable = this.scrapeProductAvailability($)
+    const asin = this.scrapeProductAsin($)
 
     const productObject: IAmazonObject = {
       asin,
-      category,
+      categories: additionalInfo.categories,
+      salesRank: additionalInfo.salesRank,
+      ratings: additionalInfo.ratings,
+      createdOn: additionalInfo.createdOn,
       isAvailable,
       merchantId,
       merchantName,
       price,
       productUrl: this.productUrl,
-      salesRank,
       title,
-    };
+    }
 
-    return productObject;
+    return productObject
   }
 
   /**
    * Scrape the Checkout Page and return the Inventory
    *
-   * @param {CheerioSelector} $
+   * @param {any} $
    * @param {string} asin
    * @returns {number}
    * @memberof AmazonScraper
    */
-  public processCheckoutPage($: CheerioSelector, asin: string): number {
-    const htmlBody = $('body').html();
+  public processCheckoutPage ($: any, asin: string): number {
+    const htmlBody = $('body').html()
 
     if (!htmlBody) {
-      throw new Error(ERROR_TYPE.NO_HTML_BODY);
+      throw new Error(ERROR_TYPE.NO_HTML_BODY)
     }
 
     if (
@@ -141,59 +150,55 @@ class AmazonScraper {
         /(Veuillez confirmer que|Confirma que quieres|Please confirm|faut que nous nous assurions que vous)/g,
       )
     ) {
-      throw new Error(ERROR_TYPE.CONFIRMATION_ERROR);
+      throw new Error(ERROR_TYPE.CONFIRMATION_ERROR)
     } else if (htmlBody && htmlBody.match(/(Votre mise à jour a)/g)) {
-      throw new Error(ERROR_TYPE.REQUEST_FAILED);
+      throw new Error(ERROR_TYPE.REQUEST_FAILED)
     }
 
     // Checking if the inventory is present on the page
     // If not it means we cannot know the exact value
-    let inventoryText: string | undefined;
-    const alertBox = $(this.alertBoxSelector);
-    const confirmText = $(this.confirmTextSelector);
+    let inventoryText: string | undefined
+    const alertBox = $(this.alertBoxSelector)
+    const confirmText = $(this.confirmTextSelector)
 
     // Checking the inventory available or if there is a limit per product
     if (alertBox.length > 0) {
-      const match = htmlBody.match(/(?:de los|a que|than the) ([0-9]+) (?:disponibles|de disponible|available)/);
-      const match2 = htmlBody.match(/(?:limite de vente de|limit of) ([0-9]+) (?:articles|per customer)/);
+      const match = htmlBody.match(/(?:de los|a que|than the) ([0-9]+) (?:disponibles|de disponible|available)/)
+      const match2 = htmlBody.match(/(?:limite de vente de|limit of) ([0-9]+) (?:articles|per customer)/)
 
       if (match) {
-        inventoryText = match[1];
+        inventoryText = match[1]
       } else if (match2) {
-        throw new Error(ERROR_TYPE.LIMIT_INVENTORY);
+        throw new Error(ERROR_TYPE.LIMIT_INVENTORY)
       }
     } else if (confirmText.length > 0) {
-      inventoryText = confirmText.text().trim();
+      inventoryText = confirmText.text().trim()
     } else {
-      const match = htmlBody.match(/\((([^)]+ (articles|article|producto|productos|items|item)))\)/g);
+      const match = htmlBody.match(/\((([^)]+ (articles|article|producto|productos|items|item)))\)/g)
       if (match) {
-        inventoryText = match[0];
+        inventoryText = match[0]
       }
     }
 
     if (!inventoryText) {
-      throw new Error(ERROR_TYPE.NO_INVENTORY);
+      throw new Error(ERROR_TYPE.NO_INVENTORY)
     }
 
     // const remainingInvDiv = $(`div[data-asin="${asin}"]`);
     // tslint:disable-next-line:no-console
     // console.log(remainingInvDiv)
-    const inventoryNumber: number = Number(inventoryText);
+    const inventoryNumber: number = Number(inventoryText)
 
-    const hiddenInventoryConfirmation = Number(
-      $(inventoryText)
-        .val()
-        .split(',')[1],
-    );
+    const hiddenInventoryConfirmation = Number($(inventoryText).val().split(',')[1])
 
     if (hiddenInventoryConfirmation !== inventoryNumber) {
-      throw new Error(ERROR_TYPE.INCORRECT_INVENTORY);
+      throw new Error(ERROR_TYPE.INCORRECT_INVENTORY)
     }
 
-    return inventoryNumber;
+    return inventoryNumber
   }
 
-  public processCartPage($: CheerioSelector, asin: string): number { 
+  public processCartPage ($: any, asin: string): number {
     const remainingInventory = $(`input[name="quantityBox"]`).val()
 
     return Number(remainingInventory)
@@ -205,21 +210,21 @@ class AmazonScraper {
    * @returns {Promise<IAmazonObject>}
    * @memberof AmazonScraper
    */
-  public async getFullProductInformation(): Promise<IAmazonObject> {
-    const productPageSelector = await this.accessPage({ url: this.productUrl, method: 'GET' });
+  public async getFullProductInformation (): Promise<IAmazonObject> {
+    const productPageSelector = await this.accessPage({ url: this.productUrl, method: 'GET' })
 
-    const productObject = await this.processProductPage(productPageSelector);
-    const formObject = await this.getAmazonFormObject(productPageSelector);
+    const productObject = await this.processProductPage(productPageSelector)
+    const formObject = await this.getAmazonFormObject(productPageSelector)
 
     if (!formObject) {
-      throw new Error(ERROR_TYPE.NO_AMAZON_FORM);
+      throw new Error(ERROR_TYPE.NO_AMAZON_FORM)
     }
 
     const inventoryPageSelector = await this.accessPage({
       formData: formObject.formData,
       method: 'POST',
       url: formObject.formUrl,
-    });
+    })
 
     const cartPage = await this.accessPage({
       method: 'GET',
@@ -229,50 +234,50 @@ class AmazonScraper {
     const productObjectWithInventory: IAmazonObject = {
       ...productObject,
       inventorySize: this.processCartPage(cartPage, productObject.asin),
-    };
+    }
 
-    return productObjectWithInventory;
+    return productObjectWithInventory
   }
 
-/**
- *  Return the Merchant Name
- *
- * @private
- * @param {CheerioSelector} $
- * @returns {(string | null)}
- * @memberof AmazonScraper
- */
-private scrapeMerchantName($: CheerioSelector): string {
+  /**
+   *  Return the Merchant Name
+   *
+   * @private
+   * @param {any} $
+   * @returns {(string | null)}
+   * @memberof AmazonScraper
+   */
+  private scrapeMerchantName ($: any): string {
     return $(this.merchantNameSelector).text().trim()
-  } 
+  }
 
   /**
    * Return The Merchant Id
    *
    * @private
-   * @param {CheerioSelector} $
+   * @param {any} $
    * @returns {(string | null)}
    * @memberof AmazonScraper
    */
-  private scrapeMerchantId($: CheerioSelector): string {
-    return $(this.merchantIdSelector).val();
+  private scrapeMerchantId ($: any): string {
+    return $(this.merchantIdSelector).val()
   }
 
   /**
    * Return the Product's Price
    *
    * @private
-   * @param {CheerioSelector} $
+   * @param {any} $
    * @returns {(string | null)}
    * @memberof AmazonScraper
    */
-  private scrapeProductPrice($: CheerioSelector): number | null {
+  private scrapeProductPrice ($: any): number | null {
     const priceId: string | undefined = _.find(this.priceSelectors, (id: any) => {
-      return $(id).length > 0;
-    });
+      return $(id).length > 0
+    })
 
     if (!priceId) {
-      return null;
+      return null
     }
 
     let price = $(priceId).text().trim()
@@ -293,20 +298,20 @@ private scrapeMerchantName($: CheerioSelector): string {
           .replace('$', '')
           .replace('€', '')
           .trim(),
-      )
-    );
+      ),
+    )
   }
 
   /**
    * Return the main sales rank
    *
    * @private
-   * @param {CheerioSelector} $
+   * @param {any} $
    * @returns {number}
    * @memberof AmazonScraper
    */
-  private scrapeSalesRank($: CheerioSelector): { mainSalesRank: number; secondarySalesRank: number } {
-    const salesRankItem = $(this.salesRankSelector).first();
+  private scrapeSalesRank ($: any): { mainSalesRank: number; secondarySalesRank: number } {
+    const salesRankItem = $(this.salesRankSelector).first()
     const salesRankElemVal = $('#SalesRank')
       .text()
       .trim()
@@ -316,79 +321,114 @@ private scrapeMerchantName($: CheerioSelector): string {
       .replace('/[.]/gm', '')
 
     // const regex = /\+|-?(\d+.\d+.?).([^\(]+)/m;
-    const regex = /[-]{0,1}[\d]*[\.]{0,1}[\d]+/m;
+    const regex = /[-]{0,1}[\d]*[\.]{0,1}[\d]+/m
 
-    const mainSalesRank = +salesRankElemVal.match(regex)[0].replace('.', '');
+    const mainSalesRank = +salesRankElemVal.match(regex)[0].replace('.', '')
 
-    const salesRankStr = $(salesRankItem)
-      .text()
-      .replace('#', '')
-      .replace('.', '')
-      .replace(',', '')
-      .replace('n°', '');
+    const salesRankStr = $(salesRankItem).text().replace('#', '').replace('.', '').replace(',', '').replace('n°', '')
 
-    const secondarySalesRank = +salesRankStr.replace('.', '');
+    const secondarySalesRank = +salesRankStr.replace('.', '')
 
     return {
       mainSalesRank,
       secondarySalesRank,
-    };
+    }
   }
 
-  private scrapeCategory($: CheerioSelector): string {
+  private scrapeAdditionalInfo ($: any): IAdditionalInformations {
+    let informations: IAdditionalInformations = {
+      categories: [],
+      ratings: [],
+      createdOn: '',
+      salesRank: [],
+    }
+    $('#productDetails_db_sections tr').each((i, elem) => {
+      const title: string = $(elem).find('.prodDetSectionEntry').text().trim().toLowerCase()
+
+      let value
+      if (title === 'best sellers rank') {
+        let rankData = []
+        const digitRegex = /#[1-9]\d*\b/gm
+        $(elem)
+          .find('td > span > span')
+          .each((i, elem) => {
+            let rank = $(elem).text().trim().match(digitRegex)[0].replace('#', '')
+            let category = $(elem).find('a').text().toLowerCase().replace('see top 100 in', '').trim()
+            let categoryLink = $(elem).find('a').attr('href')
+
+            rankData.push({ categoryLink, category, rank })
+
+            informations.categories.push(category)
+          })
+
+        informations.salesRank = rankData
+      } else if (title === 'customer reviews') {
+        value = $(elem)
+          .find('#averageCustomerReviews')
+          .text()
+          .match(/(\d+(?:\.\d+)(?=.out of 5 stars))/)[0]
+
+        const totalRatings = $(elem).find('#acrCustomerReviewText').text().replace('ratings', '').trim()
+
+        informations.ratings = { score: value, total: totalRatings }
+      } else if (title === 'date first available') {
+        value = $(elem).find('td').text().trim()
+        informations.createdOn = value
+      }
+    })
+    return informations
+  }
+
+  private scrapeCategory ($: any): string {
     const categoryMatch = $(this.categorySelector).attr('href').split('_')
 
     const category = categoryMatch[categoryMatch.length - 1].trim()
 
     return category
-  } 
+  }
 
   /**
    * Return the Product's Title
    *
    * @private
-   * @param {CheerioSelector} $
+   * @param {any} $
    * @returns {string}
    * @memberof AmazonScraper
    */
-  private scrapeProductTitle($: CheerioSelector): string {
-    return $(this.titleSelector)
-      .text()
-      .trim();
+  private scrapeProductTitle ($: any): string {
+    return $(this.titleSelector).text().trim()
   }
 
   /**
    *  Return the Product's availability
    *
    * @private
-   * @param {CheerioSelector} $
+   * @param {any} $
    * @returns {boolean}
    * @memberof AmazonScraper
    */
-  private scrapeProductAvailability($: CheerioSelector): boolean {
-    const availability = $(this.isAvailableSelector)
-      .text()
-      .trim();
+  private scrapeProductAvailability ($: any): boolean {
+    const availability = $(this.isAvailableSelector).text().trim()
 
     if (availability.match(/(Actuellement indisponible|No disponible|Unavailable)/g)) {
-      return false;
+      return false
     }
 
-    return true;
+    return true
   }
 
   /**
    * Scrape the Product's asin
    *
    * @private
-   * @param {CheerioSelector} $
+   * @param {any} $
    * @returns {string}
    * @memberof AmazonScraper
    */
-  private scrapeProductAsin($: CheerioSelector): string {
-    const asin = $('input[id="ASIN"]').val();
+  private scrapeProductAsin ($: any): string {
+    const asin = $('input[id="ASIN"]').val()
 
-    return asin;
+    return asin
   }
 
   /**
@@ -396,49 +436,49 @@ private scrapeMerchantName($: CheerioSelector): string {
    * Used to access the checkout page
    *
    * @private
-   * @param {CheerioSelector} $
+   * @param {any} $
    * @returns {(IAmazonForm | null)}
    * @memberof AmazonScraper
    */
-  private getAmazonFormObject($: CheerioSelector): IAmazonForm | null {
-    const rootUrl = Url.parse(this.productUrl);
-    const formData: any = {};
+  private getAmazonFormObject ($: any): IAmazonForm | null {
+    const rootUrl = Url.parse(this.productUrl)
+    const formData: any = {}
 
     const cartId = _.find(this.addToCardBtnSelectors, (id: any) => {
-      return $(id).length > 0;
-    });
+      return $(id).length > 0
+    })
 
     if (!cartId) {
-      return null;
+      return null
     }
 
-    const cart = $(cartId);
+    const cart = $(cartId)
 
     if (cart.length < 1) {
-      return null;
+      return null
     }
 
-    const array = $(cartId).serializeArray();
+    const array = $(cartId).serializeArray()
 
-    let formUrl = $(cartId).attr('action');
+    let formUrl = $(cartId).attr('action')
 
     // Check if the url has https and a full url.
     if (formUrl.indexOf('https://www.amazon') < 0) {
-      formUrl = formUrl.replace('https://' + rootUrl.host, '');
-      formUrl = 'https://' + rootUrl.host + formUrl;
+      formUrl = formUrl.replace('https://' + rootUrl.host, '')
+      formUrl = 'https://' + rootUrl.host + formUrl
     }
 
     _.each(array, (object: { name: string | number; value: any }) => {
-      formData[object.name] = object.value;
-    });
+      formData[object.name] = object.value
+    })
 
-    formData.quantity = this.defaultQuantity;
+    formData.quantity = this.defaultQuantity
 
     return {
       formData,
       formUrl,
-    };
+    }
   }
 }
 
-export default AmazonScraper;
+export default AmazonScraper
--- src/interfaces/amazon.ts
+++ src/interfaces/amazon.ts
@@ -1,12 +1,14 @@
 export interface IAmazonObject {
-  productUrl: string;
-  title: string;
-  asin: string;
-  category: string;
-  merchantId: string;
-  merchantName: string;
-  isAvailable: boolean;
-  price: number | null;
-  inventorySize?: number;
-  salesRank: object;
+  productUrl: string
+  title: string
+  asin: string
+  ratings: object
+  categories: object
+  createdOn: string
+  merchantId: string
+  merchantName: string
+  isAvailable: boolean
+  price: number | null
+  inventorySize?: number
+  salesRank: object
 }
